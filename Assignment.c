00977867edb525b3d33b92aecc667b5fa95e87ec
Sena Şimşek <149534466+senasimsekk@users.noreply.github.com>
2023-11-20 22:10:10 +0300

    testing

--- Assignment.c
+++ Assignment.c
@@ -2,7 +2,10 @@
 
 int main() {
     int num1, num2;
-    printf("iki tane sayı giriniz: ");
-    scanf("%d%d",&num1,&num2);
-    
+    printf("ilk sayıyı giriniz: ");
+    scanf("%d",&num1);
+    printf("ikinci sayıyı giriniz: ");
+    scanf("%d",&num2);
+
+
 }
