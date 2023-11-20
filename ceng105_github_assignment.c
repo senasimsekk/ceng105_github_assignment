2f93c45256c8a7a85ba7d23f70cfeea67d9c28eb
ceren-candemir <crncdr5@gmail.com>
2023-11-20 23:45:44 +0300

    sub eklendi

---
+++ ceng105_github_assignment.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    printf("ilk sayıyı giriniz: ");
+    scanf("%d",&num1);
+    printf("ikinci sayıyı giriniz: ");
+    scanf("%d",&num2);
+
+
+}
